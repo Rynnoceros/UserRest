e20520dcaca60471fb78286b62c3ecc1e7e71920
Rynn Oceros <jordan.soubrier@hotmail.fr>
2018-01-13 10:49:17 +0100

    Project creation

---
+++ Contexts/UserContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using UserRest.Models;
+
+namespace UserRest.Contexts
+{
+    public partial class rynnocerosContext : DbContext
+    {
+        public virtual DbSet<Pseudos> Pseudos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql(@"Host=localhost;Database=rynnoceros;Username=rynnoceros;Password=");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pseudos>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Avatar).HasColumnName("avatar");
+
+                entity.Property(e => e.Name).HasColumnName("name");
+            });
+        }
+    }
+}
---
+++ Models/Pseudos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UserRest.Models
+{
+    public partial class Pseudos
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+    }
+}
